a0e0733d80dcfea833120fc4fe516070344d39c2
Michał Płużek <michalpluzek@gmail.com>
2022-02-10 11:40:13 +0100

    feat: Add control input and methods

--- src/app/modules/todos/components/header/header.component.ts
+++ src/app/modules/todos/components/header/header.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  text: string = '';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  changeText(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+  }
+
+  addTodo(): void {
+    console.log('addTodo ' + this.text);
+  }
 }
