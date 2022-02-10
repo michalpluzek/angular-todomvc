e42837df9450afe96fe05c6382ff6adade096121
Michał Płużek <michalpluzek@gmail.com>
2022-02-10 11:26:45 +0100

    feat: Add route for todos comonent

--- src/app/modules/todos/components/todos/todos.component.ts
+++ src/app/modules/todos/components/todos/todos.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/todos/todos.module.ts
+++ src/app/modules/todos/todos.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodosComponent } from './components/todos/todos.component';
+import { TodosComponent } from 'src/app/modules/todos/components/todos/todos.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    TodosComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TodosComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class TodosModule { }
+export class TodosModule {}
