8f54cba9832c7db5c6f70411b514a63bbd48c547
Michał Płużek <michalpluzek@gmail.com>
2022-02-10 16:03:16 +0100

    feat: Add todo methods like change state, text, remove

--- src/app/modules/todos/components/todo/todo.component.ts
+++ src/app/modules/todos/components/todo/todo.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { TodosService } from 'src/app/modules/todos/services/todos.service';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { TodoInterface } from '../../types/todo.interface';
 
 @Component({
@@ -6,10 +17,47 @@ import { TodoInterface } from '../../types/todo.interface';
   templateUrl: './todo.component.html',
   styles: [],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent implements OnInit, OnChanges {
   @Input('todo') todoProps: TodoInterface = {} as TodoInterface;
+  @Input('isEditing') isEditingProps: boolean = false;
+  @Output('setEditingId') setEditingEvent: EventEmitter<string | null> =
+    new EventEmitter();
+  editingText: string = '';
+  @ViewChild('textInput') textInput!: ElementRef;
 
-  constructor() {}
+  constructor(private todosService: TodosService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.editingText = this.todoProps.text;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.isEditingProps.currentValue) {
+      setTimeout(() => {
+        this.textInput.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  setTodoInEditMode(): void {
+    this.setEditingEvent.emit(this.todoProps.id);
+  }
+
+  removeTodo(): void {
+    this.todosService.removeTodo(this.todoProps.id);
+  }
+
+  toggleTodo(): void {
+    this.todosService.toggleTodo(this.todoProps.id);
+  }
+
+  changeText(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.editingText = target.value;
+  }
+
+  changeTodo(): void {
+    this.todosService.changeTodo(this.todoProps.id, this.editingText);
+    this.setEditingEvent.emit(null);
+  }
 }
--- src/app/modules/todos/services/todos.service.ts
+++ src/app/modules/todos/services/todos.service.ts
@@ -1,3 +1,4 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { TodoInterface } from 'src/app/modules/todos/types/todo.interface';
@@ -40,5 +41,41 @@ export class TodosService {
 
   changeFilter(filterName: FilterEnum): void {
     this.filter$.next(filterName);
+  }
+
+  changeTodo(id: string, text: string): void {
+    const updatedTodos = this.todos$.getValue().map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          text,
+        };
+      }
+      return todo;
+    });
+
+    this.todos$.next(updatedTodos);
+  }
+
+  removeTodo(id: string): void {
+    const updatedTodos = this.todos$
+      .getValue()
+      .filter((todo) => todo.id !== id);
+
+    this.todos$.next(updatedTodos);
+  }
+
+  toggleTodo(id: string): void {
+    const updatedTodos = this.todos$.getValue().map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+
+    this.todos$.next(updatedTodos);
   }
 }
